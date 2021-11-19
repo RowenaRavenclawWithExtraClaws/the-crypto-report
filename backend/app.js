508b6f1a8ef2ebbab9e34ff1e6e5cad285a5d173
RowenaRavenclawWithExtraClaws <abdelrahmannasser48@gmail.com>
2021-11-19 20:19:18 +0200

    use the graphql query handler to handle incoming graphql requests

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,11 @@
 import express from "express";
 import cors from "cors";
+import graphqlReqHandler from "./graphql/routeHandler.js";
 import { portNumber } from "./utility/utility.js";
 
 const app = express();
 
 app.use(cors());
-app.use("/graphql", () => {});
+app.use("/graphql", graphqlReqHandler());
 
 app.listen(portNumber, () => console.log(`listening on port ${portNumber}`));
