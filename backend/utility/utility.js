23b54a86a37a841403a84545503c677dec6757a5
RowenaRavenclawWithExtraClaws <abdelrahmannasser48@gmail.com>
2021-11-19 20:17:55 +0200

    add pagination handler function

--- backend/utility/utility.js
+++ backend/utility/utility.js
@@ -5,3 +5,12 @@ dotenv.config();
 export const portNumber = process.env.PORT;
 export const blocksURL = process.env.BLOCKS_URL;
 export const singleBlockURL = process.env.SINGLE_BLOCK_URL;
+
+export const handlePagination = (page, pageLimit, resBody) => {
+  const firstElem = pageLimit * (page - 1);
+  const lastElem = firstElem + pageLimit;
+  const pageCount = Math.ceil(resBody.length / pageLimit);
+  const results = resBody.slice(firstElem, lastElem);
+
+  return [pageCount, results];
+};
