2b84086ad7f024115d38a2ea916eff970834089f
RowenaRavenclawWithExtraClaws <abdelrahmannasser48@gmail.com>
2021-11-21 04:59:34 +0200

    create utility.ts file to implment all the functions needed by the components

---
+++ frontend/src/utility/utility.ts
@@ -0,0 +1,35 @@
+export const getBlocks = async (queryStr: string) => {
+  try {
+    const response = await fetch("http://localhost:8000/graphql", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({ query: queryStr }),
+    });
+
+    const resBody = await response.json();
+
+    if (response.status === 200) return resBody;
+
+    return [];
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const msToDays = (time: number) =>
+  Math.ceil(time / (864 * Math.pow(10, 5)));
+
+// convert API data keys to more human-readable format (prev_block => Prev block)
+export const unslugify = (word: string) => {
+  let newWord = word[0].toUpperCase();
+
+  for (let i = 1; i < word.length; i++) {
+    if (word[i] === "_") newWord = `${newWord} `;
+    else newWord = `${newWord}${word[i]}`;
+  }
+
+  return newWord;
+};
