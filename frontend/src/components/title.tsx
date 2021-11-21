7354d8457645f8ef10a820434c64df7dc98200dc
RowenaRavenclawWithExtraClaws <abdelrahmannasser48@gmail.com>
2021-11-21 04:57:34 +0200

    create app title component

---
+++ frontend/src/components/title.tsx
@@ -0,0 +1,12 @@
+import { Typography } from "@mui/material";
+
+const Title = () => {
+  return (
+    <div className="app-title">
+      <Typography variant="h4">The Crypto Report</Typography>
+      <Typography variant="subtitle1">your daily crypto insight!</Typography>
+    </div>
+  );
+};
+
+export default Title;
