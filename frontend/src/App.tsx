fd7baba4bd5ff3cb9fec827080d944825e74a9ca
RowenaRavenclawWithExtraClaws <abdelrahmannasser48@gmail.com>
2021-11-21 05:01:21 +0200

    render all components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,55 @@
-import "./App.css";
+import { Card, CardContent, Typography, ThemeProvider } from "@mui/material";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import BlocksPagination from "./components/blocksPagination";
+import BlocksTable from "./components/blocksTable";
+import FetchIndicator from "./components/fetchIndicator";
+import Title from "./components/title";
+import {
+  selectBlocks,
+  selectPageCount,
+  setBlocks,
+  setPageCount,
+} from "./redux/blockSlice";
+import { darkTheme, queryStrings } from "./utility/constants";
+import { getBlocks } from "./utility/utility";
 
 const App = () => {
-  return <div className="App"></div>;
+  const blocks = useSelector(selectBlocks);
+  const pageCount = useSelector(selectPageCount);
+  const [fetching, setFetching] = useState(true);
+
+  const dispatch = useDispatch();
+
+  const getAndSetBlocks = async () => {
+    const result = await getBlocks(queryStrings.blocks(1));
+
+    dispatch(setBlocks(result.data.blocks));
+    dispatch(setPageCount(result.data.pageCount));
+
+    setFetching(false);
+  };
+
+  useEffect(() => {
+    getAndSetBlocks();
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  if (fetching) return <FetchIndicator />;
+
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <Title />
+      <Card>
+        <CardContent>
+          <Typography variant="h6" gutterBottom>
+            Latest blocks
+          </Typography>
+          <BlocksTable blocks={blocks} />
+          <BlocksPagination pageCount={pageCount} />
+        </CardContent>
+      </Card>
+    </ThemeProvider>
+  );
 };
 
 export default App;
