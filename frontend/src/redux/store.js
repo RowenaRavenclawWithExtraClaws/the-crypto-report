5c9ee8f62a3bede3c3fde36991b8e0893057920e
RowenaRavenclawWithExtraClaws <abdelrahmannasser48@gmail.com>
2021-11-21 05:21:26 +0200

    remove one block store

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,11 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import blocksReducer from "./blockSlice";
-import oneBlockReducer from "./oneBlockSlice";
 
 const store = configureStore({
   reducer: {
     blocks: blocksReducer,
-    block: oneBlockReducer,
   },
 });
 
